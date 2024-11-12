c3d3051199f65c1710c5c87401953ced3c65e25f
uchaa03 <uchasousaa@gmail.com>
2024-11-12 22:10:29 +0100

    V3 - Entities Listas

---
+++ kata_api/src/main/java/ucha_barrera/kata_api/entities/beer.java
@@ -0,0 +1,40 @@
+package ucha_barrera.kata_api.entities;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class beer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "breweryId")
+    private brewerie brewery;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryId")
+    private category category;
+
+    @ManyToOne
+    @JoinColumn(name = "styleId")
+    private style style;
+
+    private Float abv;
+    private Float ibu;
+    private Float upc;
+    private String filepath;
+    private String description;
+    private Long add_user;
+    private LocalDateTime last_mod;
+}
---
+++ kata_api/src/main/java/ucha_barrera/kata_api/entities/style.java
@@ -0,0 +1,36 @@
+package ucha_barrera.kata_api.entities;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class style {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryId")
+    private category categoryId;
+
+    private String style_name;
+    private LocalDate last_mod;
+
+    @OneToMany(mappedBy = "style")
+    private List<beer> beers;
+}
+//`id` int(11) NOT NULL auto_increment,
+//  `cat_id` int(11) NOT NULL default '0',
+//  `style_name` varchar(255) character set utf8 collate utf8_unicode_ci NOT NULL default '',
+//  `last_mod` datetime NOT NULL default '0000-00-00 00:00:00',
